4ac92712cae5cc50c3a24168f06ed6ecc7dfef8a
joelitok <joelnkouatchet@gmail.com>
2020-11-26 17:02:17 +0100

    deuxieme modification

--- send_mail.php
+++ send_mail.php
@@ -1,4 +1,13 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
 if (isset($_POST['email'])){
    
     echo 'pour voie si mon post fonctionne normalemet';
@@ -7,4 +16,9 @@ if (isset($_POST['email'])){
     
 }
 
-?>
+?> 
+</body>
+</html>
+
+
+
