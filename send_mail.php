f7637218c8735e3cc6c7d72d7c0bd24209462226
joelitok <joelnkouatchet@gmail.com>
2020-11-26 16:54:46 +0100

    ajouter du mail

---
+++ send_mail.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_POST['email'])){
+   
+    echo 'pour voie si mon post fonctionne normalemet';
+    $_POST['email'];
+    echo ' l email de la personne donne ceci'.$_POST['email'];
+    
+}
+
+?>
